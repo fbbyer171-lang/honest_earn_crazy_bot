acf90c8c5f63bbe24e8794b2562d29d249635c86
fbbyer171-lang <fbbyer171@gmail.com>
2026-09-10 21:42:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8980706201:AAHmK_q9vcStJiTbd-m1HGaDjbYhga3pfps"
 
 async def start(update: Update, context) -> None:
     await update.message.reply_text("Welcome to Honest Crazy Earn Bot!")
